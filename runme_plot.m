b6b12e659ae6a19efa236fdd977e0853e0750048
Michal265 <xmateas@stuba.sk>
2020-12-09 19:10:47 +0100

    matlab plotter

--- runme_plot.m
+++ runme_plot.m
@@ -5,11 +5,29 @@ clear, close, clc
 
 system('synchro.bat.lnk'); % synchronizacia dat s raspberry, treba mat pripojene raspberry na sieti, nainstalovany a nakonfigurovany WinSCP
 data = table2array(readtable('data/data.csv'));
+confi = readtable('data/conf.csv');
 
+sampling_time = table2array(confi(1,{'Hodnota'}));
 temp = data(:,1);
 input = data(:,2);
 
 %%
+t = (1:1:length(input))*sampling_time;
+
+figure(1)
+subplot(2,1,1)
+plot(t,temp,'r','linewidth',2)
+xlabel('time [s]')
+ylabel('Temperature  °C')
+grid on
+
+subplot(2,1,2)
+
+plot(t,input,'b','linewidth',2)
+xlabel('time [s]')
+ylabel('Input %')
+grid on
+ylim([-0.2,1.2])
 
 
 
