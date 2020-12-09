f0b7f422fa24016d5e5a7f501e1218b8e51c064e
Michal265 <xmateas@stuba.sk>
2020-12-09 20:44:30 +0100

    verzia 1.0

--- runme_plot.m
+++ runme_plot.m
@@ -8,26 +8,33 @@ data = table2array(readtable('data/data.csv'));
 confi = readtable('data/conf.csv');
 
 sampling_time = table2array(confi(1,{'Hodnota'}));
+ref = table2array(confi(2,{'Hodnota'}));
 temp = data(:,1);
 input = data(:,2);
 
+input(1) = [];
+temp(1) = [];
 %%
-t = (1:1:length(input))*sampling_time;
+t = (2:1:length(input)+1)*sampling_time;
 
 figure(1)
 subplot(2,1,1)
-plot(t,temp,'r','linewidth',2)
+plot(t,temp,'r','linewidth',2),hold on
+plot([t(1),t(end)],[ref,ref],'k--','linewidth',1.5)
 xlabel('time [s]')
 ylabel('Temperature  °C')
 grid on
+ylim([20,ref*1.1])
+legend('Temperature °C','Reference','location','southeast')
+
 
 subplot(2,1,2)
 
-plot(t,input,'b','linewidth',2)
+stairs(t,input*100,'b--','linewidth',2)
 xlabel('time [s]')
 ylabel('Input %')
 grid on
-ylim([-0.2,1.2])
+ylim([-20,120])
 
 
 
