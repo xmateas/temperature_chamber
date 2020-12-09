6b94e8aeafdbed9edc15c6fd3cf2cbe586a2339f
Michal265 <xmateas@stuba.sk>
2020-12-09 18:22:44 +0100

    serv

--- runme_plot.m
+++ runme_plot.m
@@ -3,6 +3,13 @@ clear, close, clc
 
 %% data gathering
 
-system('synchro.bat.lnk') % synchronizacia dat s raspberry, treba mat pripojene raspberry na sieti, nainstalovany a nakonfigurovany WinSCP
+system('synchro.bat.lnk'); % synchronizacia dat s raspberry, treba mat pripojene raspberry na sieti, nainstalovany a nakonfigurovany WinSCP
+data = table2array(readtable('data/data.csv'));
+
+temp = data(:,1);
+input = data(:,2);
+
+%%
+
 
 
