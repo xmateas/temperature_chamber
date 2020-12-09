f11a194e4147ae8cc89fa523de4bcbf041b8d38d
Michal265 <xmateas@stuba.sk>
2020-12-09 17:58:08 +0100

    Vetva pre klienta

---
+++ runme_plot.m
@@ -0,0 +1,8 @@
+%% init
+clear, close, clc
+
+%% data gathering
+
+system('synchro.bat.lnk') % synchronizacia dat s raspberry, treba mat pripojene raspberry na sieti, nainstalovany a nakonfigurovany WinSCP
+
+
